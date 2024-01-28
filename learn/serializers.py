76e616cd4a76d082522ff1b08edd8470a28088f2
Jericho Wenzel Serrano <jerichowenzel@JerichoMB.local>
2024-01-28 20:39:49 +0800

    changed the api views

--- learn/serializers.py
+++ learn/serializers.py
@@ -1,11 +1,23 @@
 from rest_framework.serializers import ModelSerializer
-from ..models import User, UserProgress, Course, CourseContent, CourseComments, Enrollment, Workouts, CorrectExerciseForm, WrongExerciseForm, Blog, BlogComments
+
+from .models import User, UserProgress, Course, CourseContent, CourseComments, Enrollment, Workouts, CorrectExerciseForm, WrongExerciseForm, Blog, BlogComments
 
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
 class UserProgressSerializer(ModelSerializer):
     class Meta:
--- learn/urls.py
+++ learn/urls.py
@@ -1,11 +1,53 @@
+from .views import *
 from django.urls import path
-from . import views
+from rest_framework.routers import DefaultRouter
 
 app_name = "learn"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("csrf/", views.csrf, name="csrf"),
 
     # API CALLS
-    path("register/", views.register, name="register")
+    path("register", RegisterView.as_view(), name="register"),
+    path("login", LoginView.as_view(), name="login"),
+    path("user", UserView.as_view(), name="user"),
+    path("logout", LogoutView.as_view(), name="logout")
 ]
+
+
+
+
+
+# Routes for our API VIEWSETS (By convention we use the model's name)
+
+# user_router = DefaultRouter()
+# user_router.register(r'User', UserView)
+
+# user_progress_router = DefaultRouter()
+# user_progress_router.register(r'UserProgress', UserProgressView)
+
+# course_router = DefaultRouter()
+# course_router.register(r'Course', CourseView)
+
+# course_content_router = DefaultRouter()
+# course_content_router.register(r'CourseContent', CourseContentView)
+
+# course_comments_router = DefaultRouter()
+# course_comments_router.register(r'CourseComments', CourseCommentsView)
+
+# enrollment_router = DefaultRouter()
+# enrollment_router.register(r'Enrollment', EnrollmentView)
+
+# workouts_router = DefaultRouter()
+# workouts_router.register(r'Workouts', WorkoutsView)
+
+# correct_exercise_form_router = DefaultRouter()
+# correct_exercise_form_router.register(r'CorrectExerciseForm', CorrectExerciseFormView)
+
+# wrong_exercise_form_router = DefaultRouter()
+# wrong_exercise_form_router.register(r'WrongExerciseForm', WrongExerciseFormView)
+
+# blog_router = DefaultRouter()
+# blog_router.register(r'Blog', BlogView)
+
+# blog_comments_router = DefaultRouter()
+# blog_comments_router.register(r'BlogComments', BlogCommentsView)
+
