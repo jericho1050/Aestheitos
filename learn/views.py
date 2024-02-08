dcaed3fb28f0ef7654f04053d9ebca5f1faf2667
Jericho Wenzel Serrano <jerichowenzel@JerichoMB.local>
2024-02-08 15:11:48 +0800

    unit test typo

--- learn/views.py
+++ learn/views.py
@@ -2,7 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+# from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 # from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .serializers import *
