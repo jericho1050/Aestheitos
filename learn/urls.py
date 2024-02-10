ee7fb9ba606687857464bb9666c032a219743398
Jericho Wenzel Serrano <jerichowenzel@JerichoMB.local>
2024-02-11 03:08:22 +0800

    Refactoring Views.py using OOP concepts and URLs

--- learn/urls.py
+++ learn/urls.py
@@ -11,59 +11,26 @@ urlpatterns = [
     path("user", UserView.as_view(), name="user"),
     path("logout", LogoutView.as_view(), name="logout"),
 
-    # path('courses', CourseList.as_view(), name='course-list'),
-    # path('courses/<int:pk>', CourseList.as_view(), name='course-detail')
+
+    path('courses', CourseList.as_view(), name='course-list'),
+    path('course/<int:course_id>', CourseDetail.as_view(), name='course-detail'),
+    path('course-content/<int:course_id>', CourseContentDetail.as_view(), name='CRU-course-content'),
+    path('course/<int:course_id>/workouts', WorkoutList.as_view(), name="course-workouts"),
+    path('course/workout/<int:course_id>', WorkoutDetail.as_view(), name="course-workout")
+    
 
 
 
 
 
 
-    path("courses/list", CoursesView.as_view(), name="browse"),
-    path("courses/<int:course_id>/content", CourseContentview.as_view(), name="course_content"),
-    path("courses/<int:course_id>/workout", WorkoutsView.as_view(), name="course_workout"),
-    path("workout/<int:workout_id>/correct_form", CorrectExerciseFormView.as_view(), name="correct_form"),
-    path("workout/<int:workout_id>/wrong_form", WrongExerciseFormView.as_view(), name="wrong_form" ),
-    path("courses/<int:course_id>/comments", CourseCommentsView.as_view(), name="course_comments")
+
+    # path("courses/<int:course_id>/content", CourseContentview.as_view(), name="course_content"),
+    # path("courses/<int:course_id>/workout", WorkoutsView.as_view(), name="course_workout"),
+    # path("workout/<int:workout_id>/correct_form", CorrectExerciseFormView.as_view(), name="correct_form"),
+    # path("workout/<int:workout_id>/wrong_form", WrongExerciseFormView.as_view(), name="wrong_form" ),
+    # path("courses/<int:course_id>/comments", CourseCommentsView.as_view(), name="course_comments")
 
 ]
 
-
-
-
-
-# Routes for our API VIEWSETS (By convention we use the model's name)
-
-# user_router = DefaultRouter()
-# user_router.register(r'User', UserView)
-
-# user_progress_router = DefaultRouter()
-# user_progress_router.register(r'UserProgress', UserProgressView)
-
-# course_router = DefaultRouter()
-# course_router.register(r'Course', CourseView)
-
-# course_content_router = DefaultRouter()
-# course_content_router.register(r'CourseContent', CourseContentView)
-
-# course_comments_router = DefaultRouter()
-# course_comments_router.register(r'CourseComments', CourseCommentsView)
-
-# enrollment_router = DefaultRouter()
-# enrollment_router.register(r'Enrollment', EnrollmentView)
-
-# workouts_router = DefaultRouter()
-# workouts_router.register(r'Workouts', WorkoutsView)
-
-# correct_exercise_form_router = DefaultRouter()
-# correct_exercise_form_router.register(r'CorrectExerciseForm', CorrectExerciseFormView)
-
-# wrong_exercise_form_router = DefaultRouter()
-# wrong_exercise_form_router.register(r'WrongExerciseForm', WrongExerciseFormView)
-
-# blog_router = DefaultRouter()
-# blog_router.register(r'Blog', BlogView)
-
-# blog_comments_router = DefaultRouter()
-# blog_comments_router.register(r'BlogComments', BlogCommentsView)
 
