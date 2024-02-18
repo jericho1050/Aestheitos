78174176f8d185544cf2ebdcf128ecbb3160c539
Jericho Wenzel Serrano <jerichowenzel@JerichoMB.local>
2024-02-19 01:23:05 +0800

    refactored the API generic view again and created a custom method in serializers.py and models.py for the implementation of DRY.

--- learn/serializers.py
+++ learn/serializers.py
@@ -1,83 +1,192 @@
 from rest_framework.serializers import ModelSerializer
-
-from .models import User, UserProgress, Course, CourseContent, CourseComments, Enrollment, Workouts, CorrectExerciseForm, WrongExerciseForm, Blog, BlogComments
+from rest_framework.exceptions import AuthenticationFailed
+from django.shortcuts import get_object_or_404
+import inspect
+from .models import *
+from .helpers import is_valid_ownership
 
 #  In Django, a serializer is a way to convert complex data types, like Django models, into Python native datatypes that can then be easily rendered into JSON, XML, or other content types. This process is called serialization.
+
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
+        fields = "__all__"
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        password = validated_data.pop('password', None)
+        password = validated_data.pop("password", None)
         instance = self.Meta.model(**validated_data)
         if password is not None:
             instance.set_password(password)
         instance.save()
         return instance
 
+
 class UserProgressSerializer(ModelSerializer):
     class Meta:
         model = UserProgress
-        fields = '__all__'
+        fields = "__all__"
+
 
 class CourseSerializer(ModelSerializer):
     class Meta:
         model = Course
-        fields = '__all__'
-        read_only_fields = ['created_by']
+        fields = "__all__"
+        read_only_fields = ["created_by"]
+
+    def save_with_auth_user(self, user, pk, update=False):
+
+        if update:
+
+            if "status" in self.validated_data and not user.is_staff:
+                raise AuthenticationFailed("Only staff can change the status")
+
+            if not user.is_superuser and self.instance.created_by != user:
+                raise AuthenticationFailed("Not allowed to modify")
+
+            self.save()
+            return
+        self.save(created_by=user)
 
 
 class CourseContentSerializer(ModelSerializer):
     class Meta:
         model = CourseContent
-        fields = '__all__'
-        read_only_fields = ['course']
+        fields = "__all__"
+        read_only_fields = ["course"]
+
+    def save_with_auth_user(self, user, pk):
+        if self.instance.course.created_by != user:
+            raise AuthenticationFailed("Not allowed to modify")
+        self.save()
 
 
 class CourseCommentsSerializer(ModelSerializer):
     class Meta:
         model = CourseComments
-        fields = '__all__'
-        read_only_fields = ['course', 'comment_by']
+        fields = "__all__"
+        read_only_fields = ["course", "comment_by"]
+
+    def save_with_auth_user(self, user, pk, update=False):
+
+        if update:
+
+            if self.instance.comment_by != user:
+                raise AuthenticationFailed("Not allowed to modify comment")
+            self.save()
+            return
+
+        course = get_object_or_404(Course, id=pk)
+        self.save(course=course, comment_by=user)
+
 
 class EnrollmentSerializer(ModelSerializer):
     class Meta:
         model = Enrollment
-        fields = '__all__'
-        read_only_fields = ['user', 'course']
+        fields = "__all__"
+        read_only_fields = ["user", "course"]
+
+    def save_with_auth_user(self, user, pk):
+        course = get_object_or_404(Course, id=pk)
+        if is_valid_ownership(user, course.id):
+            raise AuthenticationFailed("Not allowed to enroll in their own course")
+        self.save(user=user, course=course)
+
+
+
+
+
 
 class WorkoutsSerializer(ModelSerializer):
     class Meta:
         model = Workouts
-        fields = '__all__'
-        read_only_fields = ['course']
+        fields = "__all__"
+        read_only_fields = ["course"]
+
+    def save_with_auth_user(self, user, pk, update=False):
+
+        if update:
+            if self.instance.course.created_by != user:
+                raise AuthenticationFailed("Not allowed to modify")
+            self.save()
+            return
+
+        course = get_object_or_404(Course, id=pk)
+        if course.created_by != user:
+            raise AuthenticationFailed("Not allowed to create")
+        self.save(course=course)
+
 
 class CorrectExerciseFormSerializer(ModelSerializer):
     class Meta:
         model = CorrectExerciseForm
-        fields = '__all__'
-        read_only_fields = ['workout']
+        fields = "__all__"
+        read_only_fields = ["workout"]
+
+    def save_with_auth_user(self, user, pk, update=False):
+
+        if update:
+            if not is_valid_ownership(user, self.instance.workout.course.id):
+                raise AuthenticationFailed("Not allowed to modify")
+            self.save()
+            return
+
+        workout = get_object_or_404(Workouts, id=pk)
+        if not is_valid_ownership(user, workout.course.id):
+            raise AuthenticationFailed("Not allowed to create")
+        self.save(workout=workout)
+
 
 class WrongExerciseFormSerializer(ModelSerializer):
     class Meta:
         model = WrongExerciseForm
-        fields = '__all__'
-        read_only_fields = ['workout']
+        fields = "__all__"
+        read_only_fields = ["workout"]
+
+    def save_with_auth_user(self, user, pk, update=False):
+
+        if update:
+            if not is_valid_ownership(user, self.instance.workout.course.id):
+                raise AuthenticationFailed("Not allowed to modify")
+            self.save()
+            return
+
+        workout = get_object_or_404(Workouts, id=pk)
+        if not is_valid_ownership(user, workout.course.id):
+            raise AuthenticationFailed("Not allowed to create")
+        self.save(workout=workout)
+
 
 class BlogSerializer(ModelSerializer):
     class Meta:
         model = Blog
-        fields = '__all__'
-        read_only_fields = ['author']
+        fields = "__all__"
+        read_only_fields = ["author"]
+
+    def save_with_auth_user(self, user, pk, update=False):
+
+        if update:
+            if self.instance.author != user:
+                raise AuthenticationFailed("Not allowed to modify")
+            self.save()
+            return
+
+        self.save(author=user)
+
 
 class BlogCommentsSerializer(ModelSerializer):
     class Meta:
         model = BlogComments
-        fields = '__all__'
-        read_only_fields =  ['blog', 'comment_by']
+        fields = "__all__"
+        read_only_fields = ["blog", "comment_by"]
 
+    def save_with_auth_user(self, user, pk, update=False):
+
+        if update:
+            if self.instance.comment_by != user:
+                raise AuthenticationFailed("Not allowed to modify")
+            self.save()
+
+        blog = get_object_or_404(Blog, id=pk)
+        self.save(blog=blog, comment_by=user)
