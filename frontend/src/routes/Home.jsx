c6aeccbed50588161aa5c92a16b0b8737c024235
Jericho Wenzel Serrano <jerichowenzel@JerichoMB.local>
2024-03-19 22:33:00 +0800

    added a mysterious text animation for the homepage

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,19 +1,40 @@
 import { Box, Container, Grid, ThemeProvider, Typography, createTheme, responsiveFontSizes, useMediaQuery } from "@mui/material";
 import { useSpring, animated } from "@react-spring/web";
+import { useState } from "react";
 
 
+// basis for this animation Mysterious Text 
+// https://codesandbox.io/p/sandbox/mysterious-text-animation-with-react-spring-vhj66
+// By imhuyqn
 
-// 
+const MysteriousText = ({ children, ...props }) => {
+    const matches = useMediaQuery(theme => theme.breakpoints.up('md'))
+    const animation = i =>
+        useSpring({ opacity: 1, from: { opacity: 0 }, delay: Math.random() * 650 });
+    return (
+        <Typography variant={matches ? 'h3' : 'h4'} sx={{ color: '#5A5A5A', fontWeight: 600, fontFamily: '"Helvetica Neue"', textAlign: 'left' }} {...props}>
+            {children.split("").map((item, index) => (
+                <animated.span key={index} style={animation(index)}>
+                    {item}
+                </animated.span>
+            ))}
+        </Typography>
+    );
+};
 export function Home() {
+    const [animationFinished, setAnimationFinished] = useState(false);
+
     let theme = createTheme();
     theme = responsiveFontSizes(theme);
-    const matches = useMediaQuery(theme => theme.breakpoints.up('md'))
     const springs = useSpring({
         from: { x: -1000, y: -window.innerHeight * 0.35 },
         to: async (next, cancel) => {
             await next({ x: 0, y: -window.innerHeight * 0.35 });
             await next({});
-        }
+        },
+        onRest: () => setAnimationFinished(true)
+
+
     })
     return (<>
         <Box sx={{ margin: -1, padding: 0, position: 'relative' }}>
@@ -21,9 +42,12 @@ export function Home() {
             <animated.div style={{ ...springs }}>
                 <Container fixed={true} maxWidth={false} sx={{ display: 'flex', backgroundColor: 'rgba(255, 255, 255, 0.5)', padding: { xs: '4%', sm: '6%' }, paddingLeft: { xs: '8%' }, paddingRight: { xs: '8%' }, width: { xs: '69%' } }} className="container-homepage">
                     <ThemeProvider theme={theme}>
-                        <Typography variant={matches ? 'h3' : 'h4'} sx={{ color: '#5A5A5A', fontWeight: 600, fontFamily: '"Helvetica Neue"', textAlign: 'left' }}>
-                            Challenge Yourself, Unleash Your Potential: Start Your Free Calisthenics Journey Today.
-                        </Typography>
+                        {/* <Typography variant={matches ? 'h3' : 'h4'} sx={{ color: '#5A5A5A', fontWeight: 600, fontFamily: '"Helvetica Neue"', textAlign: 'left' }}>
+                        </Typography> */}
+                        {animationFinished &&
+                            <MysteriousText>
+                                Challenge Yourself, Unleash Your Potential: Start Your Free Calisthenics Journey Today.
+                            </MysteriousText>}
                     </ThemeProvider>
                 </Container>
             </animated.div>
@@ -35,8 +59,8 @@ export function Home() {
             <Grid container justifyContent={'center'}>
                 <Grid item paddingTop={'10vh'}>
                     <ThemeProvider theme={theme}>
-                        <Typography variant={'h3'} sx={{fontWeight: 'bold'}}>
-                            Courses            
+                        <Typography variant={'h3'} sx={{ fontWeight: 'bold' }}>
+                            Courses
                         </Typography>
                     </ThemeProvider>
                 </Grid>
