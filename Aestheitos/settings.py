0d1938e3c615f95ce839c4beb6d8d723f8a693f4
Jericho Serrano <118679068+jericho1050@users.noreply.github.com>
2024-12-22 00:48:19 +0800

    chore: change getenv with environ.get instead

--- Aestheitos/settings.py
+++ Aestheitos/settings.py
@@ -115,11 +115,11 @@ WSGI_APPLICATION = "Aestheitos.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": "5432",
+        "NAME": os.environ.get("POSTGRES_DB", "mydatabase"),
+        "USER": os.environ.get("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", ""),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
