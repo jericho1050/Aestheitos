10489d6840fd1fd47e9887157d5ab794f56a81ba
Jericho Serrano <118679068+jericho1050@users.noreply.github.com>
2024-12-22 00:13:43 +0800

    chore: update django.yml back with it's postgres iamge and change env key variable names

--- Aestheitos/settings.py
+++ Aestheitos/settings.py
@@ -114,11 +114,11 @@ WSGI_APPLICATION = "Aestheitos.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("PGDATABASE"),
-        "USER": os.getenv("PGUSER"),
-        "PASSWORD": os.getenv("PGPASSWORD"),
-        "HOST": os.getenv("PGHOST"),
-        "PORT": os.getenv("PGPORT"),
+        "NAME": os.getenv('POSTGRES_DB'),
+        "USER": os.getenv('POSTGRES_USER'),
+        "PASSWORD": os.getenv('POSTGRES_PASSWORD'),
+        "HOST": os.getenv('POSTGRES_HOST'),
+        "PORT": "5432",
     }
 }
 
