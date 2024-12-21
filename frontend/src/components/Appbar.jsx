3abad47b28855d9ba353eb2312e4542ac66b3b36
Jericho Serrano <118679068+jericho1050@users.noreply.github.com>
2024-12-22 04:34:01 +0800

    fix: filter is nota functino problem in appbar

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -65,7 +65,7 @@ function ResponsiveAppBar() {
   const isAuthenticated = token['access'] !== null;
   const navigate = useNavigate();
   const { user, courses } = useLoaderData(); // loader is in root.jsx
-  const userCourses = courses.filter(
+  const userCourses = courses?.filter(
     (course) => course.created_by === user.user_id && course.status !== 'P'
   ); // just return THE user's or instructor's courses for notifcation purposes.
   const profilePic = useAtomValue(profilePictureAtom || '');
