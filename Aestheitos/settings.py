d924f4f8abbeace6f603221df6e3c63ed345500b
Jericho Serrano <118679068+jericho1050@users.noreply.github.com>
2024-12-23 03:36:48 +0800

    style: no comma lols

--- Aestheitos/settings.py
+++ Aestheitos/settings.py
@@ -96,7 +96,7 @@ SESSION_COOKIE_SECURE = True
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware'    
+    'whitenoise.middleware.WhiteNoiseMiddleware',    
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
