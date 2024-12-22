66cc7720af77d5f18c8ce3fafb89f11266992f8b
Jericho Serrano <118679068+jericho1050@users.noreply.github.com>
2024-12-22 21:52:26 +0800

    chore: NOT FINAL fix but adding loadenv in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,33 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5';
 import { createRequire } from 'node:module';
-const require = createRequire( import.meta.url );
+const require = createRequire(import.meta.url);
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), ckeditor5({ theme: require.resolve('@ckeditor/ckeditor5-theme-lark') })
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/tests/setup.js',
-    css: true,
-  },
-  publicDir: 'public' // use standard 'public' directory
-
-})
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [
+      react(),
+      ckeditor5({ theme: require.resolve('@ckeditor/ckeditor5-theme-lark') }),
+    ],
+    define: {
+      'import.meta.env.VITE_API_URL': JSON.stringify(
+        process.env.VITE_API_URL || env.VITE_API_URL
+      ),
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/tests/setup.js',
+      css: true,
+    },
+    publicDir: 'public', // use standard 'public' directory
+    preview: {
+      port: 5173,
+    },
+  };
+});
