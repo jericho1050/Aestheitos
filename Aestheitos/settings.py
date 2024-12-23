757e406a42003f74afa50185d37de2342425be6d
Jericho Serrano <118679068+jericho1050@users.noreply.github.com>
2024-12-24 03:03:56 +0800

    refacotr: added https://seashell-app-lfqfa.ondigitalocean.app to ALOWED, CORS

--- Aestheitos/settings.py
+++ Aestheitos/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = [
     "ambitious-stone-00aad4900.4.azurestaticapps.net",
     "aestheitos.vercel.app",
     "stingray-app-24qhl.ondigitalocean.app",
+    "seashell-app-lfqfa.ondigitalocean.app"
 ]
 
 
@@ -65,6 +66,7 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:5173",
     "https://ambitious-stone-00aad4900.4.azurestaticapps.net",
     "https://aestheitos.vercel.app",
+    "https://seashell-app-lfqfa.ondigitalocean.app"
 ]
 CORS_EXPOSE_HEADERS = [
     "Content-Type",
@@ -77,11 +79,13 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://ambitious-stone-00aad4900.4.azurestaticapps.net",
     "https://aestheitos.vercel.app",
+    "https://seashell-app-lfqfa.ondigitalocean.app"
 ]
 CSRF_TRUSTED_ORIGINS = [
     "https://ambitious-stone-00aad4900.4.azurestaticapps.net",
     "https://aestheitos.vercel.app",
     "https://stingray-app-24qhl.ondigitalocean.app",
+    "https://seashell-app-lfqfa.ondigitalocean.app"
 ]
 # CSRF_COOKIE_HTTPONLY = True
 # CSRF_COOKIE_SECURE = False
